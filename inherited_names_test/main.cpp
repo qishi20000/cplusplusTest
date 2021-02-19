c6cc9634e20fa8b806d2aac326dd1c90d71913f4
qishi20000 <qishi20000@gmail.com>
2021-02-19 13:29:44 +0800

    增加inherited names 代码

---
+++ inherited_names_test/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+class Bass
+{
+public:
+	virtual void mf1() = 0;
+	virtual void mf1(int) { std::cout << "base mf1" << std::endl; }
+	virtual void mf2() { std::cout << "base mf2" << std::endl; }
+	void mf3() { std::cout << "base mf3 non" << std::endl; }
+	void mf3(double) { std::cout << "base mf3" << std::endl; }
+
+private:
+	int x;
+};
+
+class Derived :public Bass
+{
+public:
+	virtual void mf1() { std::cout << "derived mf1" << std::endl; }
+	void mf3() { std::cout << "derived mf3" << std::endl; }
+	void mf4() { std::cout << "derived mf4" << std::endl; }
+};
+
+int main()
+{
+	Derived d;
+	int x = 0;
+	d.mf1();
+	//d.mf1(x);  //错误！因为derived::mf1遮掩了Base::mf1
+	d.mf2();
+	d.mf3();
+	//d.mf3(x); //错误！因为derived::mf3遮掩了Base::mf3
+}
